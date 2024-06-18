8b8867f7c93f7dda726b3f2b2da42be65405821d
JetWen <killer500k@gmail.com>
2024-06-18 22:59:17 +0800

    Test TensoeFlow Operations of Array element

---
+++ python/05_pad_sequences.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pprint as pp
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+comment1 = [1, 2, 3, 4]
+comment2 = [1, 2, 3, 4, 5, 6, 7]
+comment3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+x_train = np.array([comment1, comment2, comment3], dtype = object)
+print(), pp.pprint(x_train)
+
+#unsert 0 to left of array to make array have same length
+x_test =  pad_sequences(x_train)
+print(), pp.pprint(x_test)
+
+#insert specific Value to array to make array have same length
+x_test =  pad_sequences(x_train, value = 255)
+print(), pp.pprint(x_test)
+
+#insert 0 to right of array to make array have same length
+x_test =  pad_sequences(x_train, padding = "post")
+print(), pp.pprint(x_test)
+
+#Cut the array, only remain last 3 element
+x_test =  pad_sequences(x_train, maxlen = 3)
+print(), pp.pprint(x_test)
+
+
+#Cut the array, only reamin head of 3 element
+x_test =  pad_sequences(x_train, maxlen = 3, truncating = "post")
+print(), pp.pprint(x_test)
