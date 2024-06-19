25e29d3cdae91bb678c0f3b48bb5547c1f46743b
JetWen <killer500k@gmail.com>
2024-06-19 11:03:29 +0800

    Practicing TensorFlow Reshape API

--- python/11_reshape.py
+++ python/11_reshape.py
@@ -3,3 +3,38 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.system("clear")
 
 import tensorflow as tf
+import numpy as np
+
+a = tf.random.normal([10, 28, 28, 3])
+print(a)
+print(a.shape)      # Shape
+print(a.ndim)       # Dimension
+
+b = tf.reshape(a, [10, 784, 3])  
+print(b)
+print(b.shape)      # Shape
+print(b.ndim)       # Dimension
+
+# "-1" will auto reshape
+c = tf.reshape(a, [10, -1, 3])  
+print(c)
+print(c.shape)      # Shape
+print(c.ndim)       # Dimension
+
+d = tf.reshape(a, [10, 784 * 3])  
+print(d)
+print(d.shape)      # Shape
+print(d.ndim)       # Dimension
+
+# "-1" will auto reshape
+e = tf.reshape(a, [10, -1])  
+print(e)
+print(e.shape)      # Shape
+print(e.ndim)       # Dimension
+
+
+
+
+
+
+
