3ece7b8327275a9a82ce2202348d328cf22ebcfd
JetWen <killer500k@gmail.com>
2024-06-20 21:39:10 +0800

    Use Numpy API to compute sin/cos/tan

---
+++ Numpy/14_sin_cos_tan.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+print("-" * 5, "sin", "-" * 5)
+
+val = np.sin(np.pi / 6)
+print(val)
+print(np.round(val, 1))
+
+print("-" * 5, "sin", "-" * 5)
+val = np.cos(np.pi / 3)
+print(val)
+print(np.round(val, 1))
+
+print("-" * 5, "sin", "-" * 5)
+val = np.tan(np.pi / 4)
+print(val)
+print(np.round(val, 1))
+
