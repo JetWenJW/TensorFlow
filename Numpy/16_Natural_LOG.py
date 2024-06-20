8176913c8c0410060c0ee7d7eee7abff573aae38
JetWen <killer500k@gmail.com>
2024-06-20 21:52:02 +0800

    Use Numpy API to compute Log & review log

---
+++ Numpy/16_Natural_LOG.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+n = [1, 2, 3, 4, 5]
+
+val = np.log(1)
+print(val)
+print("Array log")
+val = np.log(n)
+print(val)
+
+
+#2
+val = np.log2(16)
+print(val)
+
+n = [1, 2, 4, 8, 32, 128]
+val = np.log2(n)
+print(val)
+
+#10
+val = np.log10(1000)
+print(val)
+
+n = [1, 100, 1000, 10000, 100000, 1000000]
+val = np.log10(n)
+print(val)
+
