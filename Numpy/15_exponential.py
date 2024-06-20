632f88bd4883e11b6b006ccdc33a0dee4bad95a0
JetWen <killer500k@gmail.com>
2024-06-20 21:46:49 +0800

    Use Numpy API to compute natural exponential

---
+++ Numpy/15_exponential.py
@@ -0,0 +1,26 @@
+import numpy as np
+n = [1, 2, 3, 4, 5]
+
+val = np.e
+print(val)
+
+val = np.exp(1)
+print(val)
+print(np.round(val))
+
+print("*" * 40)
+val = np.exp(n)
+print(val)
+print(np.round(val))
+
+val = np.exp2(1)
+print(val)
+print(np.round(val))
+
+val = np.power(3, 4)
+print(val)
+print(np.round(val))
+
+val = np.power(3, n)
+print(val)
+print(np.round(val))
