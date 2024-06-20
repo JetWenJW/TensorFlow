1760084ae731b671c4fb3a34e29afd84309d6ff8
JetWen <killer500k@gmail.com>
2024-06-20 21:30:00 +0800

    Use Numpy API to Operations Array(+, -, *, /)

---
+++ Numpy/12_compute.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+a = np.arange(10)
+
+print(a)
+print(a + 5)
+print(a - 5)
+print(a * 5)
+print(a ** 2)
+print(a / 2)
+print(a % 2)
+
+print("-" * 40)
+
+print(np.add(a, 5))
+print(np.subtract(a, 5))
+print(np.multiply(a, 5))
+print(np.power(a, 2))
+print(np.divide(a, 2))
+print(np.negative(a))
+print(np.mod(a, 2))
+
+
+
+
